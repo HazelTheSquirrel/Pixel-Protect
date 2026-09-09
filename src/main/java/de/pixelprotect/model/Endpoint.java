4d6655d8c26466805f570ec066d2b73627071c5f
HazelTheSquirrel <hazelgin01@gmail.com>
2026-09-09 15:31:23 +0200

    Give ground-item endpoints stable entity identity

--- src/main/java/de/pixelprotect/model/Endpoint.java
+++ src/main/java/de/pixelprotect/model/Endpoint.java
@@ -1,37 +1,14 @@
 package de.pixelprotect.model;
 
 import org.bukkit.Location;
-
 import java.util.UUID;
 
-public record Endpoint(EndpointType type, UUID entityId, UUID playerId, String world, int x, int y, int z, String label) {
-    public static Endpoint player(UUID uuid, String name, Location location) {
-        return new Endpoint(EndpointType.PLAYER, null, uuid, location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), name);
-    }
-
-    public static Endpoint block(Location location, String label) {
-        return new Endpoint(EndpointType.BLOCK_CONTAINER, null, null, location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), label);
-    }
-
-    public static Endpoint entity(UUID uuid, Location location, String label) {
-        return new Endpoint(EndpointType.MINECART, uuid, null, location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), label);
-    }
-
-    public static Endpoint ground(Location location) {
-        return new Endpoint(EndpointType.GROUND, null, null, location.getWorld().getName(), location.getBlockX(), location.getBlockY(), location.getBlockZ(), "Boden");
-    }
-
-    public static Endpoint system() {
-        return new Endpoint(EndpointType.SYSTEM, null, null, null, 0, 0, 0, "System");
-    }
-
-    public String identity() {
-        return switch (type) {
-            case PLAYER -> "player:" + playerId;
-            case BLOCK_CONTAINER -> "block:" + world + ":" + x + ":" + y + ":" + z;
-            case MINECART -> "entity:" + entityId;
-            case GROUND -> "ground:" + world + ":" + x + ":" + y + ":" + z;
-            case SYSTEM -> "system";
-        };
-    }
+public record Endpoint(EndpointType type,UUID entityId,UUID playerId,String world,int x,int y,int z,String label){
+    public static Endpoint player(UUID uuid,String name,Location l){return new Endpoint(EndpointType.PLAYER,null,uuid,l.getWorld().getName(),l.getBlockX(),l.getBlockY(),l.getBlockZ(),name);}
+    public static Endpoint block(Location l,String label){return new Endpoint(EndpointType.BLOCK_CONTAINER,null,null,l.getWorld().getName(),l.getBlockX(),l.getBlockY(),l.getBlockZ(),label);}
+    public static Endpoint entity(UUID uuid,Location l,String label){return new Endpoint(EndpointType.MINECART,uuid,null,l.getWorld().getName(),l.getBlockX(),l.getBlockY(),l.getBlockZ(),label);}
+    public static Endpoint ground(Location l){return ground(null,l);}
+    public static Endpoint ground(UUID entity,Location l){return new Endpoint(EndpointType.GROUND,entity,null,l.getWorld().getName(),l.getBlockX(),l.getBlockY(),l.getBlockZ(),"Boden");}
+    public static Endpoint system(){return new Endpoint(EndpointType.SYSTEM,null,null,null,0,0,0,"System");}
+    public String identity(){return switch(type){case PLAYER->"player:"+playerId;case BLOCK_CONTAINER->"block:"+world+":"+x+":"+y+":"+z;case MINECART->"entity:"+entityId;case GROUND->entityId==null?"ground:"+world+":"+x+":"+y+":"+z:"+entityId:"ground:"+entityId;case SYSTEM->"system";};}
 }
